e9c64ca5f242aa3eed9bf46ead662d02c22fddc3
Matt7x7-9 <takayuk1.matt@gmail.com>
2026-03-23 19:27:25 +0900

    Fix repository URL in plugin config

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -7,7 +7,7 @@ const config: ExternalPluginConfig = {
     title: 'OFP Route',
     description: 'OFPのGPXルートをWindy上に表示し、各ウェイポイントの気象データを取得します。',
     author: 'Takayuki Matsumoto',
-    repository: 'https://github.com/windycom/windy-plugin-template',
+    repository: 'https://github.com/Matt7x7-9/windy-ofp-plugin',
     desktopUI: 'rhpane',
     mobileUI: 'fullscreen',
     routerPath: '/ofp-route',
